300091c3484052a6bd32fe451f28392482c4c4f2
borisshi7 <144968842+borisshi7@users.noreply.github.com>
2024-04-21 00:02:21 -0700

    Add files via upload

---
+++ dataStructure_RecentUpload.js
@@ -0,0 +1,58 @@
+import { MongoClient } from "mongodb";
+import { createClient } from "redis";
+
+const mongoClient = await MongoClient.connect("mongodb://localhost:27017");
+const photos = mongoClient.db("photoStock").collection("photos");
+
+const redisClient = createClient();
+redisClient.on("error", function (error) {
+  console.error(error);
+});
+await redisClient.connect();
+
+/*
+    1. Photographer's Uploads (List):
+    - Key: "recentUploads:artist_Id"
+    - Elements: photo IDs of selected and uploaded photos by a photographer
+    - Description: This DB is used to upload the contents to the database, for the photographer to select and upload the photos.
+      The photos could be restricted to a certain number, in this example, 10 photos for batch uploading at one time. 
+*/
+
+//Add (push) selected pictures to the Redis DB
+async function addRecentUpload(artist_id, photo_id) {
+    const key = `recentUploads:${artist_id}`;
+    await redisClient.lPush(key, photo_id.toString());
+    await redisClient.lTrim(key, 0, 9);
+  }
+
+//Get (pop) selected pictures from the Redis DB
+async function getRecentUploads(artist_id) {
+  const key = `recentUploads:${artist_id}`;
+  const photoIds = await redisClient.lRange(key, 0, -1);
+  const recentPhotos = await photos.find({ photo_id: { $in: photoIds.map(Number) } }).toArray();
+  return recentPhotos;
+}
+
+
+// As an example, I searched for the photos about street photography and added them to the recent uploads list.
+const newPhotos = await photos.find({
+  "tags.tagName": "Street Photography"
+}).toArray();
+console.log('Number of new photos:', newPhotos.length);
+
+for (const newPhoto of newPhotos) {
+  console.log('New Photo:', newPhoto);
+  await addRecentUpload(newPhoto.artist.artist_id, newPhoto.photo_id);
+}
+
+if (newPhotos.length > 0) {
+  const recentUploads = await getRecentUploads(newPhotos[0].artist.artist_id);
+  console.log('Recent Uploads:', recentUploads);
+} else {
+  console.log('No new photos found that match the criteria.');
+}
+
+redisClient.FLUSHDB(); //This is for deleting the contents of the DB for not making the DB too big for storing in cache. 
+
+await mongoClient.close();
+await redisClient.quit();
